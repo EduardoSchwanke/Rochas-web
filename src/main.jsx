0b7a30603caa3c794698f3a08e4db1734c86b670
Eduardo Schwanke <carvalhoe089@gmail.com>
2022-08-24 08:07:27 -0300

    a

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { App } from './App'
 
 import './index.css'
-import { UserContextProvider } from "./contexts/UserContext";
-import { NavContextProvider } from "./contexts/NavContext";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
 
