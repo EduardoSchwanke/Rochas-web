46a190f8b15007650e8947d629580f8b3e74f0bb
Eduardo Schwanke <carvalhoe089@gmail.com>
2022-08-25 10:34:44 -0300

    home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { BsArrowRightCircle } from "react-icons/bs";
 import { IoChevronDown } from "react-icons/io5";
 import { GiMountaintop } from "react-icons/gi";
+import { GiMountains } from "react-icons/gi";
 
 import 'swiper/css'
 import 'swiper/css/navigation'
@@ -47,18 +48,28 @@ export function Home() {
                 </SwiperSlide>
 
                 <SwiperSlide className='h-[100vh]'>
-                    <section id="metaforfica" className=" bg-green-100 z-10">
-                    <Div img={metaforficaImg}> 
+                    <section id="metaforfica" className="bg-green-100 z-10  relative">
+                        <Div img={metaforficaImg} className="relative flex items-center flex-col"> 
+                            <GiMountains className="absolute top-[90px] left-3 text-3xl"/>
+                            <BsArrowRightCircle className='absolute top-[90px] right-3 text-3xl'/>
                             <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Metaforfica</h1>
+                            <p className='p-4 pl-16 leading-5 text-justify'>This button component has a primary state that changes its color. When setting the primary prop to true, we are swapping out its background and text color.</p>
+                            <IoChevronDown className='absolute bottom-5 text-white text-3xl animate-bounce'/>
                         </Div>
+                        <div className='w-[2px] h-[80vh] bg-zinc-600 absolute left-6 top-[140px] z-20'></div>
                     </section>
                 </SwiperSlide>
 
                 <SwiperSlide className='h-[100vh]'>
-                    <section id="sedimentar" className=" bg-red-100 z-10">
-                    <Div img={sedimentarImg}> 
+                    <section id="sedimentar" className=" bg-red-100 z-10 relative">
+                        <Div img={sedimentarImg} className="relative flex items-center flex-col"> 
+                            <GiMountaintop className="absolute top-[90px] left-3 text-3xl"/>
+                            <BsArrowRightCircle className='absolute top-[90px] right-3 text-3xl'/>
                             <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Sedimentar</h1>
+                            <p className='p-4 pl-16 leading-5 text-justify'>This button component has a primary state that changes its color. When setting the primary prop to true, we are swapping out its background and text color.</p>
+                            <IoChevronDown className='absolute bottom-5 text-white text-3xl animate-bounce'/>
                         </Div>
+                        <div className='w-[2px] h-[80vh] bg-zinc-600 absolute left-6 top-[140px] z-20'></div>
                     </section>
                 </SwiperSlide>
             </Swiper>
