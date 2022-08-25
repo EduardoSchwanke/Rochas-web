696d6a657e2d5389637da40775d0fce558dd163f
Eduardo Schwanke <carvalhoe089@gmail.com>
2022-08-25 08:41:52 -0300

    swiper slide add home

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,42 +1,43 @@
-import { useState } from "react"
-import { useEffect } from "react"
+import { Swiper, SwiperSlide } from 'swiper/react'
+
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import { Navigation, Pagination } from 'swiper';
 
 export function Home() {
-
-    const [screenMove, setScreenMove] = useState()
-
-    useEffect(() => {
-        setScreenMove(window.scrollY)
-    },[window.scrollY])
-
-    if(screenMove > 200){
-        console.log('dds')
-    }
-
     return (
         <>
             <header className={`hidden md:flex px-6 h-[56px] w-full justify-between items-center bg-zinc-50 drop-shadow-md z-20 fixed`}>
                 <span>Ignea</span>
                 <span>v</span>
-            </header>
+            </header> 
 
-            <section className=" bg-yellow-100 z-10">
-                <div className="h-[100vh] w-[100vw] bg-ignea bg-no-repeat bg-bottom [background-size:200%]"> 
-                    <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Ignea</h1>
-                </div>
-            </section>
+            <Swiper modules={[Navigation, Pagination]} navigation={false}  pagination={false} slidesPerView={1} direction={'vertical'} spaceBetween={0} className="h-[100vh]">
+                <SwiperSlide className='h-[100vh]'>
+                    <section id="ignea" className=" bg-yellow-100 z-10">
+                        <div className="h-[100vh] w-[100vw] bg-ignea bg-no-repeat bg-bottom [background-size:200%]"> 
+                            <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Ignea</h1>
+                        </div>
+                    </section>
+                </SwiperSlide>
 
-            <section className=" bg-green-100 z-10">
-                <div className="h-[100vh] w-[100vw] bg-metaforfica bg-no-repeat bg-bottom [background-size:200%]"> 
-                    <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Metaforfica</h1>
-                </div>
-            </section>
+                <SwiperSlide className='h-[100vh]'>
+                    <section id="metaforfica" className=" bg-green-100 z-10">
+                        <div className="h-[100vh] w-[100vw] bg-metaforfica bg-no-repeat bg-bottom [background-size:200%]"> 
+                            <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Metaforfica</h1>
+                        </div>
+                    </section>
+                </SwiperSlide>
 
-            <section className=" bg-red-100 z-10">
-                <div className="h-[100vh] w-[100vw] bg-sedimentar bg-no-repeat bg-bottom [background-size:200%]"> 
-                    <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Sedimentar</h1>
-                </div>
-            </section>
+                <SwiperSlide className='h-[100vh]'>
+                    <section id="sedimentar" className=" bg-red-100 z-10">
+                        <div className="h-[100vh] w-[100vw] bg-sedimentar bg-no-repeat bg-bottom [background-size:200%]"> 
+                            <h1 className='text-center text-3xl pt-20 drop-shadow-lg text-zinc-700'>Sedimentar</h1>
+                        </div>
+                    </section>
+                </SwiperSlide>
+            </Swiper>
         </>
     )
 }
